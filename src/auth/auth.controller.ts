a186a71838870e87337c0b7e28c9e6c56d53baa8
mrx7355608 <mrx7355608@gmail.com>
2024-12-28 22:49:59 +0500

    created a signup route

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, UsePipes } from '@nestjs/common';
+import { ZodValidationPipe } from 'src/utils/ZodValidationPipeline';
+import { SignupDTO, signupSchema } from './auth.validations';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    @Post('signup')
+    @UsePipes(new ZodValidationPipe(signupSchema))
+    async signup(data: SignupDTO) {
+        console.log(data);
+        return { message: 'hello' };
+    }
+}
